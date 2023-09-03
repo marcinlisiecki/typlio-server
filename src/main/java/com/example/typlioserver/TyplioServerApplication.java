ede36499bc7393a2918a1865f986803e78b849fe
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-09-03 15:40:06 +0200

    Initial

---
+++ src/main/java/com/example/typlioserver/TyplioServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.typlioserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyplioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyplioServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/typlioserver/TyplioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.typlioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyplioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
