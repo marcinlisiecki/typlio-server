9a61c98aac389eb481a8d69e07cc54ac698b20c6
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-09-26 01:28:00 +0200

    Change error messages to english

--- src/main/java/com/example/typlioserver/common/constants/ErrorMessages.java
+++ src/main/java/com/example/typlioserver/common/constants/ErrorMessages.java
@@ -2,17 +2,17 @@ package com.example.typlioserver.common.constants;
 
 public class ErrorMessages {
 
-    public static String EMAIL_ALREADY_EXISTS = "Użytkownik z takim adresem email już istnieje";
-    public static String USERNAME_ALREADY_EXISTS = "Użytkownik z taką nazwą już istnieje";
-    public static String USER_NOT_FOUND = "Nie znaleziono użytkownika";
-    public static String BAD_CREDENTIALS = "Niepoprawna nazwa użytkownika lub/i hasło";
-    public static String USER_NOT_AUTHENTICATED = "Musisz być zalogowany, żeby wykonać tę akcję";
+    public static String EMAIL_ALREADY_EXISTS = "User with this email address already exists";
+    public static String USERNAME_ALREADY_EXISTS = "User with this username already exists";
+    public static String USER_NOT_FOUND = "User not found";
+    public static String BAD_CREDENTIALS = "Incorrect username and/or password";
+    public static String USER_NOT_AUTHENTICATED = "You must be logged in to perform this action";
 
-    public static String SPEED_TEST_NOT_FOUND = "Nie znaleziono testu szybkości pisania";
+    public static String SPEED_TEST_NOT_FOUND = "Speed test not found";
 
-    public static String INSUFFICIENT_PERMISSIONS = "Brak uprawnień";
+    public static String INSUFFICIENT_PERMISSIONS = "Insufficient permissions";
 
-    public static String DEFAULT_ERROR = "Błąd serwera";
+    public static String DEFAULT_ERROR = "Server error";
 
     public static String PASSWORD_RESET_TOKEN_NOT_FOUND = "Invalid token";
     public static String PASSWORD_RESET_TOKEN_EXPIRED = "Token expired";
